e32ca5c7bb837c55d5f8c3113ec816e7b7bd7847
Nikita <garret613@gmail.com>
2020-10-21 21:39:38 +0300

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import TravelItem from "./components/travelItem/travelItem";
 import "./components/travelItem/travelItem.css";
+import Login from "./components/login/login";
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="travelItem">
-      <TravelItem />
+      <Router>
+      <Link to="/">Home</Link>        
+       <Link to="/about">About</Link>        
+       <Route exact path="/" component={TravelItem} />        
+       <Route exact path="/about" component={Login} />       
+      </Router>
     </div>
   );
 }
